f3d98b6e81134e69a8d8c8a534b73a782230aac5
Yotam-D <d.yotam@gmail.com>
2022-07-26 13:04:29 +0300

    connect sanity client with the About Data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,23 @@ import { useState, useEffect } from 'react';
 import {motion} from 'framer-motion';
 import './About.scss';
 import { images } from '../../constants';
+import {urlFor, client} from '../../client';
 
-const abouts = [
-  {title: 'Web Development', description: 'I am a good web developer.', imgURL:images.about01 },
-  {title: 'Web Design', description: 'I am a good web developer.', imgURL:images.about02 },
-  {title: 'Web UI/UX', description: 'I am a good web developer.', imgURL:images.about03 },
-  {title: 'Web Animation', description: 'I am a good web developer.', imgURL:images.about04 },
-]
+// const abouts = [
+//   {title: 'Web Development', description: 'I am a good web developer.', imgURL:images.about01 },
+//   {title: 'Web Design', description: 'I am a good web developer.', imgURL:images.about02 },
+//   {title: 'Web UI/UX', description: 'I am a good web developer.', imgURL:images.about03 },
+//   {title: 'Web Animation', description: 'I am a good web developer.', imgURL:images.about04 },
+// ]
 
 const About = () => {
+  const [abouts,setAbouts] = useState([])
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query)
+      .then((data)=>setAbouts(data))
+  }, [])
+  
   return (
     <>
     <h2 className='head-text'>
@@ -25,7 +33,7 @@ const About = () => {
           className = 'app__profile-item'
           key = {about.title+index}
           >
-            <img src={about.imgURL} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{marginTop : 20}}>{about.title}</h2>
             <p className='bold-text' style={{marginTop : 10}}>{about.description}</p>
           </motion.div>
